68d03c616b2b2edbf52d5bb4c20475157077c81d
Darkk-kami <71535947+Darkk-kami@users.noreply.github.com>
2024-07-23 22:40:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh
 sudo groupadd docker
 sudo usermod -aG docker "$USER"
+newgrp docker
 sudo rm ./get-docker.sh
 sudo apt install -y jq net-tools nginx
 
